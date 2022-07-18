8184841a70ad8674bc5822518b47bc0b2d59d1c1
Anshul00Sharma <42910335+Anshul00Sharma@users.noreply.github.com>
2022-07-18 23:32:02 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
